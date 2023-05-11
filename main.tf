8f23aba3853d27885b011f10f4d91f45216d35fc
Zack Siri <me@zacksiri.dev>
2023-05-11 16:44:51 +0700

    update hetzner module

--- main.tf
+++ main.tf
@@ -3,15 +3,20 @@ provider "hcloud" {
 }
 
 locals {
+  user                 = "root"
   bootstrap_ip_address = "10.0.1.253"
+  topology = {
+    for index, node in var.cluster_topology :
+    node.name => node
+  }
 }
 
 resource "ssh_resource" "trust_token" {
   host         = [for obj in hcloud_server.bootstrap_node.network : upper(obj.ip)][0]
   bastion_host = hcloud_server.bastion.ipv4_address
 
-  user         = "root"
-  bastion_user = "root"
+  user         = local.user
+  bastion_user = local.user
 
   private_key         = tls_private_key.bastion_key.private_key_openssh
   bastion_private_key = tls_private_key.terraform_cloud.private_key_openssh
@@ -22,18 +27,18 @@ resource "ssh_resource" "trust_token" {
 }
 
 resource "ssh_resource" "cluster_join_token" {
-  count        = var.cluster_size
+  for_each     = local.topology
   host         = [for obj in hcloud_server.bootstrap_node.network : upper(obj.ip)][0]
   bastion_host = hcloud_server.bastion.ipv4_address
 
-  user         = "root"
-  bastion_user = "root"
+  user         = local.user
+  bastion_user = local.user
 
   private_key         = tls_private_key.bastion_key.private_key_openssh
   bastion_private_key = tls_private_key.terraform_cloud.private_key_openssh
 
   commands = [
-    "lxc cluster add ${var.cluster_name}-node-${format("%02d", count.index + 1)} | sed '1d; /^$/d'"
+    "lxc cluster add ${var.cluster_name}-node-${each.key} | sed '1d; /^$/d'"
   ]
 }
 
@@ -83,10 +88,10 @@ resource "hcloud_server" "bootstrap_node" {
 
   connection {
     type                = "ssh"
-    user                = "root"
+    user                = local.user
     host                = local.bootstrap_ip_address
     private_key         = tls_private_key.bastion_key.private_key_openssh
-    bastion_user        = "root"
+    bastion_user        = local.user
     bastion_host        = hcloud_server.bastion.ipv4_address
     bastion_private_key = tls_private_key.terraform_cloud.private_key_openssh
   }
@@ -126,9 +131,9 @@ resource "hcloud_server" "bootstrap_node" {
 }
 
 resource "hcloud_server" "nodes" {
-  count              = var.cluster_size
+  for_each           = local.topology
   image              = var.image
-  name               = "${var.cluster_name}-node-${format("%02d", count.index + 1)}"
+  name               = "${var.cluster_name}-node-${each.key}"
   location           = var.location
   server_type        = var.node_size
   ssh_keys           = [hcloud_ssh_key.bastion.name]
@@ -148,18 +153,18 @@ resource "hcloud_server" "nodes" {
 
   connection {
     type                = "ssh"
-    user                = "root"
-    host                = "10.0.1.${count.index + 1}"
+    user                = local.user
+    host                = "10.0.1.${each.value.id}"
     private_key         = tls_private_key.bastion_key.private_key_openssh
-    bastion_user        = "root"
+    bastion_user        = local.user
     bastion_host        = hcloud_server.bastion.ipv4_address
     bastion_private_key = tls_private_key.terraform_cloud.private_key_openssh
   }
 
   provisioner "file" {
     content = templatefile("${path.module}/templates/lxd-join.yml.tpl", {
-      ip_address   = "10.0.1.${count.index + 1}"
-      join_token   = ssh_resource.cluster_join_token[count.index].result
+      ip_address   = "10.0.1.${each.value.id}"
+      join_token   = ssh_resource.cluster_join_token[each.key].result
       storage_size = var.storage_size
     })
 
@@ -170,7 +175,6 @@ resource "hcloud_server" "nodes" {
     inline = [
       "cloud-init status --wait",
       "lxd init --preseed < /tmp/lxd-join.yml",
-      "shutdown -r +1"
     ]
   }
 
@@ -187,6 +191,100 @@ resource "hcloud_server" "nodes" {
 
   network {
     network_id = hcloud_network.cluster_vpc.id
-    ip         = "10.0.1.${count.index + 1}"
+    ip         = "10.0.1.${each.value.id}"
+  }
+}
+
+resource "ssh_resource" "node_detail" {
+  for_each = local.topology
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  host         = [for obj in hcloud_server.bootstrap_node.network : upper(obj.ip)][0]
+  bastion_host = hcloud_server.bastion.ipv4_address
+
+  user         = local.user
+  bastion_user = local.user
+
+  private_key         = tls_private_key.bastion_key.private_key_openssh
+  bastion_private_key = tls_private_key.terraform_cloud.private_key_openssh
+
+  commands = [
+    "lxc cluster show ${hcloud_server.nodes[each.key].name}"
+  ]
+}
+
+resource "terraform_data" "reboot" {
+  for_each = local.topology
+
+  input = {
+    user                        = local.user
+    node_name                   = hcloud_server.nodes[each.key].name
+    bastion_private_key         = tls_private_key.bastion_key.private_key_openssh
+    bastion_public_ip           = hcloud_server.bastion.ipv4_address
+    node_private_ip             = [for obj in hcloud_server.nodes[each.key].network : upper(obj.ip)][0]
+    terraform_cloud_private_key = tls_private_key.terraform_cloud.private_key_openssh
+    commands = contains(yamldecode(ssh_resource.node_detail[each.key].result).roles, "database-leader") ? ["echo Node is database-leader restarting later", "sudo shutdown -r +2"] : [
+      "sudo shutdown -r +1"
+    ]
+  }
+
+  connection {
+    type                = "ssh"
+    user                = self.input.user
+    host                = self.input.node_private_ip
+    private_key         = self.input.bastion_private_key
+    bastion_user        = self.input.user
+    bastion_host        = self.input.bastion_public_ip
+    bastion_private_key = self.input.terraform_cloud_private_key
+    timeout             = "10s"
+  }
+
+  provisioner "remote-exec" {
+    inline = self.input.commands
+  }
+}
+
+resource "terraform_data" "removal" {
+  for_each = local.topology
+
+  input = {
+    user                        = local.user
+    node_name                   = hcloud_server.nodes[each.key].name
+    bastion_private_key         = tls_private_key.bastion_key.private_key_openssh
+    bastion_public_ip           = hcloud_server.bastion.ipv4_address
+    bootstrap_node_private_ip   = [for obj in hcloud_server.bootstrap_node.network : upper(obj.ip)][0]
+    terraform_cloud_private_key = tls_private_key.terraform_cloud.private_key_openssh
+    commands = contains(yamldecode(ssh_resource.node_detail[each.key].result).roles, "database-leader") ? ["echo ${var.protect_leader ? "Node is database-leader cannot destroy" : "Tearing it all down"}", "exit ${var.protect_leader ? 1 : 0}"] : [
+      "lxc cluster evac --force ${hcloud_server.nodes[each.key].name}",
+      "lxc cluster remove ${hcloud_server.nodes[each.key].name}"
+    ]
+  }
+
+  depends_on = [
+    hcloud_server.bastion,
+    hcloud_server.bootstrap_node,
+    hcloud_network.cluster_vpc,
+    hcloud_network_subnet.cluster_subnet,
+    hcloud_firewall.bastion_firewall,
+    hcloud_firewall.bastion_firewall
+  ]
+
+  connection {
+    type                = "ssh"
+    user                = self.input.user
+    host                = self.input.bootstrap_node_private_ip
+    private_key         = self.input.bastion_private_key
+    bastion_user        = self.input.user
+    bastion_host        = self.input.bastion_public_ip
+    bastion_private_key = self.input.terraform_cloud_private_key
+    timeout             = "10s"
+  }
+
+  provisioner "remote-exec" {
+    when   = destroy
+    inline = self.input.commands
   }
 }
