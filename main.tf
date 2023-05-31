fb7398c3cc6e83bdd3f63d6ff33e7a0f90dc11af
Zack Siri <me@zacksiri.dev>
2023-05-31 22:21:17 +0700

    use force remove
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- main.tf
+++ main.tf
@@ -258,8 +258,7 @@ resource "terraform_data" "removal" {
     bootstrap_node_private_ip   = [for obj in hcloud_server.bootstrap_node.network : upper(obj.ip)][0]
     terraform_cloud_private_key = tls_private_key.terraform_cloud.private_key_openssh
     commands = contains(yamldecode(ssh_resource.node_detail[each.key].result).roles, "database-leader") ? ["echo ${var.protect_leader ? "Node is database-leader cannot destroy" : "Tearing it all down"}", "exit ${var.protect_leader ? 1 : 0}"] : [
-      "lxc cluster evac --force ${hcloud_server.nodes[each.key].name}",
-      "lxc cluster remove ${hcloud_server.nodes[each.key].name}"
+      "lxc cluster remove --force --yes ${hcloud_server.nodes[each.key].name}"
     ]
   }
 
