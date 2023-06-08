5c555e3e2342a170ece72392cc9e4654cd973693
Zack Siri <me@zacksiri.dev>
2023-06-08 13:17:46 +0700

    update example

--- examples/compute-with-instellar/main.tf
+++ examples/compute-with-instellar/main.tf
@@ -11,8 +11,8 @@ module "compute" {
   hcloud_token = var.hcloud_token
   cluster_name = local.cluster_name
   cluster_topology = [
-    {id = 1, name = "apple", size = "cpx11"},
-    {id = 2, name = "watermelon", size = "cpx11"},
+    {id = 1, name = "01", size = "cpx11"},
+    {id = 2, name = "02", size = "cpx11"},
   ]
   node_size = "cpx11"
   storage_size = 30
@@ -32,7 +32,7 @@ module "instellar" {
   auth_token      = var.instellar_auth_token
   cluster_name    = local.cluster_name
   region          = module.compute.region
-  provider_name   = locals.provider_name
+  provider_name   = local.provider_name
   cluster_address = module.compute.cluster_address
   password_token  = module.compute.trust_token
 
